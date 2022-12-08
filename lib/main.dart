6182ea92d7ccc34d7afa996f3b648f99efc97583
Chivu Marius <chivumarius@yahoo.com>
2022-12-08 05:50:31 +0200

    Authentication Logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:tiktok_flutter/constants.dart';
 // import 'package:tiktok_flutter/views/screens/auth/login_screen.dart';
 import 'package:tiktok_flutter/views/screens/auth/signup_screen.dart';
 
-void main() {
+void main() async {
+  // ♦ Initialize the Widget:
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // ♦ Initialize the App on Firebase:
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // ♦ Constructor
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This Widget is the Root of your Application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       // ♦ Removing the "Debug" Banner:
       debugShowCheckedModeBanner: false,
       title: 'TikTok Clone',
