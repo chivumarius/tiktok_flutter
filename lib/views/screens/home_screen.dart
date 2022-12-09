b6a0f713c6d552fac1aa0c70e4f9b032593e9622
Chivu Marius <chivumarius@yahoo.com>
2022-12-09 14:05:20 +0200

    Bottom Tab Bar

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,14 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_flutter/constants.dart';
+import 'package:tiktok_flutter/views/widgets/custom_icon.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // ♦ Variable:
+  int pageIdx = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (idx) {
+          setState(() {
+            pageIdx = idx;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: backgroundColor,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.white,
+        currentIndex: pageIdx,
+
+        // ♦ Items List:
+        items: const [
+          // ♦ "Home" Item:
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, size: 30),
+            label: 'Home',
+          ),
+
+          // ♦ "Search" Item:
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search, size: 30),
+            label: 'Search',
+          ),
+
+          // ♦ "Add Icon" Item:
+          BottomNavigationBarItem(
+            icon: CustomIcon(),
+            label: '',
+          ),
+
+          // ♦ "Messages" Item:
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message, size: 30),
+            label: 'Messages',
+          ),
+
+          // ♦ "Profile" Item:
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, size: 30),
+            label: 'Profile',
+          ),
+        ],
+      ),
       body: Center(
-        child: Text('Home Screen'),
+        child: pages[pageIdx],
       ),
     );
   }
